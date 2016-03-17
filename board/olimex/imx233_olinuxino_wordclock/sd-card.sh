605d5de1e70c36dd2d0f67e9955fcaed1a2e60c7
Aaron Larisch <aaron@openlarry.de>
2016-03-17 22:01:17 +0100

    Use squashfs and overlayfs with remote update function in mind

--- board/olimex/imx233_olinuxino_wordclock/sd-card.sh
+++ board/olimex/imx233_olinuxino_wordclock/sd-card.sh
@@ -1,32 +1,11 @@
 #!/bin/sh
 set -e
 
-if [ -e $1/sd-card.img ]; then
-	rm $1/sd-card.img
-fi
+# copy partition table
+gzip -d -c board/olimex/imx233_olinuxino_wordclock/sd-card.img.gz > $1/sd-card.img
 
-
-fssize=$(stat --printf="%s" $1/rootfs.ext2)
-imgsize=$(expr $fssize / 512 + 4096)
-
-dd if=/dev/zero of=$1/sd-card.img bs=512 count=$imgsize
-
-# echo "n
-# p
-# 1
-# 
-# +1M
-# t
-# 53
-# n
-# p
-# 2
-# 
-# 
-# w
-# " | fdisk -b 512 -H 62 -S 62 $1/sd-card.img
-
-dd if=board/olimex/imx233_olinuxino_wordclock/mbr.bin of=$1/sd-card.img bs=512
-dd if=$1/uboot-env.bin of=$1/sd-card.img bs=512 seek=512
-dd if=$1/u-boot.sd of=$1/sd-card.img bs=512 seek=2048
-dd if=$1/rootfs.ext2 of=$1/sd-card.img bs=512 seek=4096
+# copy partitions
+dd if=$1/uboot-env.bin of=$1/sd-card.img bs=512 seek=512 conv=notrunc
+dd if=$1/u-boot.sd of=$1/sd-card.img bs=512 seek=2048 conv=notrunc
+dd if=$1/uImage of=$1/sd-card.img bs=512 seek=6144 conv=notrunc
+dd if=$1/rootfs.squashfs of=$1/sd-card.img bs=512 seek=26624 conv=notrunc
