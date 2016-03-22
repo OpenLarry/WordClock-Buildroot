75c68e0fa23541e8f539e26d3f728a84d92c43c1
Aaron Larisch <aaron@openlarry.de>
2016-03-22 18:56:42 +0100

    Update to Buildroot 2016.02 and Linux 4.4.6

--- board/olimex/imx233_olinuxino_wordclock/sd-card.sh
+++ board/olimex/imx233_olinuxino_wordclock/sd-card.sh
@@ -7,5 +7,5 @@ gzip -d -c board/olimex/imx233_olinuxino_wordclock/sd-card.img.gz > $1/sd-card.i
 # copy partitions
 dd if=$1/uboot-env.bin of=$1/sd-card.img bs=512 seek=512 conv=notrunc
 dd if=$1/u-boot.sd of=$1/sd-card.img bs=512 seek=2048 conv=notrunc
-dd if=$1/uImage of=$1/sd-card.img bs=512 seek=6144 conv=notrunc
+dd if=$1/uImage.imx23-olinuxino of=$1/sd-card.img bs=512 seek=6144 conv=notrunc
 dd if=$1/rootfs.squashfs of=$1/sd-card.img bs=512 seek=26624 conv=notrunc
