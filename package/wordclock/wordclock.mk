bce7509a8ed1ce415a4ac15b288758c4daff27b8
Aaron Larisch <aaron@openlarry.de>
2016-06-11 19:00:09 +0200

    Add Lua support

--- package/wordclock/wordclock.mk
+++ package/wordclock/wordclock.mk
@@ -7,7 +7,7 @@
 WORDCLOCK_VERSION = v0.3
 WORDCLOCK_SITE = git@github.com:OpenLarry/WordClock.git
 WORDCLOCK_SITE_METHOD = git
-WORDCLOCK_DEPENDENCIES = host-vala libglib2 json-glib sdl sdl_image glib-networking libsoup libgee
+WORDCLOCK_DEPENDENCIES = host-vala libglib2 json-glib sdl sdl_image glib-networking libsoup libgee luajit
 
 define WORDCLOCK_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 package/wordclock/S50wordclock \
