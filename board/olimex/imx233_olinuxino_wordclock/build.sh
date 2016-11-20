4d451ac2a64552176741d28bc401239c8dae0195
Aaron Larisch <aaron@openlarry.de>
2016-11-20 16:47:22 +0100

    Disable devinput lirc driver

--- board/olimex/imx233_olinuxino_wordclock/build.sh
+++ board/olimex/imx233_olinuxino_wordclock/build.sh
@@ -5,3 +5,7 @@ GIT=$(git describe --dirty)
 DATE=$(date -R)
 
 echo "$GIT $DATE" > $TARGET_DIR/build
+
+if [ -f $TARGET_DIR/etc/lirc/lircd.conf.d/devinput.lircd.conf ]; then
+	mv $TARGET_DIR/etc/lirc/lircd.conf.d/devinput.lircd.conf $TARGET_DIR/etc/lirc/lircd.conf.d/devinput.lircd.dist
+fi
