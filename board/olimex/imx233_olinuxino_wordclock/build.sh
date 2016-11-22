8c22604d2d298930d008a67ada3e73fd17865d78
Aaron Larisch <aaron@openlarry.de>
2016-11-22 14:35:22 +0100

    Enable static gzip and disable unused modules in nginx

--- board/olimex/imx233_olinuxino_wordclock/build.sh
+++ board/olimex/imx233_olinuxino_wordclock/build.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 set -e
 
+# save filesystem version
 GIT=$(git describe --dirty)
 DATE=$(date -R)
 
 echo "$GIT $DATE" > $TARGET_DIR/build
 
+# disable default remote
 if [ -f $TARGET_DIR/etc/lirc/lircd.conf.d/devinput.lircd.conf ]; then
 	mv $TARGET_DIR/etc/lirc/lircd.conf.d/devinput.lircd.conf $TARGET_DIR/etc/lirc/lircd.conf.d/devinput.lircd.dist
 fi
+
+# compress web directory
+find output/target/usr/html/ -type f ! -iname "*.gz" | xargs -r gzip -fk9
