407dfec4440b0b047c8686e5dd1e3874f546e1e1
Aaron Larisch <aaron@openlarry.de>
2017-02-03 13:43:15 +0100

    Add wireless networks web interface

---
+++ board/olimex/imx233_olinuxino_wordclock/skeleton/usr/html/wirelessnetworks/script.js
@@ -0,0 +1,114 @@
+jQuery(document).ready(function($) {
+	function get() {
+		$.ajax({
+			url: 'http://'+location.hostname+':8080/wirelessnetworks',
+			type: 'GET',
+			success: function(result) {
+				for(var id in result) {
+					setNetwork(id, result[id]);
+				}
+			},
+			error: function() {
+				setTimeout(getSettings, 1000);
+			}
+		});
+	}
+	
+	function edit() {
+		var form = $(this).parent().parent().parent();
+		var id = form.parent().parent().find('.id').text();
+		
+		var data = {};
+		form.serializeArray().map(function(x){data[x.name] = x.value;});
+		data.enabled = data.enabled == 'true';
+		
+		form.find('input, button').prop('disabled', true);
+		
+		$.ajax({
+			url: 'http://'+location.hostname+':8080/wirelessnetworks/'+id,
+			type: 'PUT',
+			data: JSON.stringify(data),
+			processData: false,
+			contentType: 'application/json',
+			success: function(result) {
+			},
+			error: function(result) {
+				alert(result.responseText);
+			},
+			complete: function() {
+				form.find('input, button').prop('disabled', false);
+			}
+		});
+	}
+	
+	function remove() {
+		var form = $(this).parent().parent().parent();
+		var id = form.parent().parent().find('.id').text();
+		
+		form.find('input, button').prop('disabled', true);
+		
+		$.ajax({
+			url: 'http://'+location.hostname+':8080/wirelessnetworks/'+id,
+			type: 'DELETE',
+			success: function(result) {
+				$('#network-'+id).remove();
+			},
+			error: function(result) {
+				alert(result.responseText);
+			},
+			complete: function() {
+				form.find('input, button').prop('disabled', false);
+			}
+		});
+	}
+	
+	function add() {
+		var form = $(this).parent().parent().parent();
+		
+		var data = {};
+		form.serializeArray().map(function(x){data[x.name] = x.value;});
+		data.enabled = data.enabled == 'true';
+		
+		form.find('input, button').prop('disabled', true);
+		
+		$.ajax({
+			url: 'http://'+location.hostname+':8080/wirelessnetworks',
+			type: 'POST',
+			data: JSON.stringify(data),
+			processData: false,
+			contentType: 'application/json',
+			success: function(id) {
+				setNetwork(id, data);
+				form[0].reset();
+			},
+			error: function(result) {
+				alert(result.responseText);
+			},
+			complete: function() {
+				form.find('input, button').prop('disabled', false);
+			}
+		});
+	}
+	
+	
+	function setNetwork(id, network) {
+		var elem = $('#network-'+id);
+		if(elem.length == 0) {
+			elem = $('#network').clone();
+			elem.find('.edit').click(edit);
+			elem.find('.remove').click(remove);
+		}
+			
+		elem.attr('id','network-'+id);
+		elem.find('.id').text(id);
+		elem.find('.ssid').val(network.ssid);
+		elem.find('.psk').val(network.psk);
+		elem.find('.enabled').prop('checked',network.enabled);
+		if(network.current) elem.find('.current').show();
+		elem.show();
+		elem.appendTo('#networks');
+	}
+	
+	get();
+	$('.add').click(add);
+});
