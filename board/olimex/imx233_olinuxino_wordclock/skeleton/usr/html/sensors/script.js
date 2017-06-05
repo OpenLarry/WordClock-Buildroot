82d21b5534e0b651f5cb1761b6cf54691f6b8e36
Aaron Larisch <aaron@openlarry.de>
2017-06-05 20:35:37 +0200

    Add estimation of power consumption to hardware info

--- board/olimex/imx233_olinuxino_wordclock/skeleton/usr/html/sensors/script.js
+++ board/olimex/imx233_olinuxino_wordclock/skeleton/usr/html/sensors/script.js
@@ -16,11 +16,11 @@ jQuery(document).ready(function($) {
 					var obj = result[g][i];
 					
 					if(typeof(obj['value']) == "boolean" && obj['value']) {
-						$('#'+i+' .true').show();
-						$('#'+i+' .false').hide();
+						$('.'+i+' .true').show();
+						$('.'+i+' .false').hide();
 					}else if(typeof(obj['value']) == "boolean" && !obj['value']) {
-						$('#'+i+' .true').hide();
-						$('#'+i+' .false').show();
+						$('.'+i+' .true').hide();
+						$('.'+i+' .false').show();
 					}else{
 						for(var v in obj) {
 							var value_p = obj[v];
@@ -43,11 +43,11 @@ jQuery(document).ready(function($) {
 							if(["nice","irq","iowait"].indexOf(v) >= 0) value_v = rest += value_v;
 							if(v == "total") value_v = mem_v += value_v;
 							if(v == "available") value_v = mem_v -= value_v;
-							if(["user","nice","system","idle","irq","softirq","iowait","total","available"].indexOf(v) >= 0) value_v = Math.round(value_v);
+							if(["user","nice","system","idle","irq","softirq","iowait","total","available","power-consumption"].indexOf(v) >= 0) value_v = Math.round(value_v);
 							
 							
-							$('#'+i+' .progress-bar.'+v).css({ width: value_p+"%"});
-							$('#'+i+' .value.'+v).text(value_v);
+							$('.'+i+' .progress-bar.'+v).css({ width: value_p+"%"});
+							$('.'+i+' .value.'+v).text(value_v);
 						}
 					}
 				}
