a613b1858fba29a2a603d9d9045dfafdc67d1f30
Aaron Larisch <aaron@openlarry.de>
2018-08-07 22:08:31 +0200

    Improve update procedure

--- board/olimex/imx233_olinuxino_wordclock/sd-card.sh
+++ board/olimex/imx233_olinuxino_wordclock/sd-card.sh
@@ -9,3 +9,7 @@ dd if=$1/uboot-env.bin of=$1/sd-card.img bs=512 seek=512 conv=notrunc
 dd if=$1/u-boot.sd of=$1/sd-card.img bs=512 seek=2048 conv=notrunc
 dd if=$1/uImage.imx23-olinuxino of=$1/sd-card.img bs=512 seek=6144 conv=notrunc
 dd if=$1/rootfs.squashfs of=$1/sd-card.img bs=512 seek=26624 conv=notrunc
+
+# generate update image
+dd if=$1/uImage.imx23-olinuxino of=$1/update.bin bs=512
+dd if=$1/rootfs.squashfs of=$1/update.bin bs=512 seek=8192
