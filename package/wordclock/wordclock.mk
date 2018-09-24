db5ab6226f293c0bf6e30846bb70c7bce0eb9bdf
Aaron Larisch <aaron@openlarry.de>
2018-09-24 15:25:50 +0200

    Use wpa_client library

--- package/wordclock/wordclock.mk
+++ package/wordclock/wordclock.mk
@@ -7,7 +7,7 @@
 WORDCLOCK_VERSION = custom
 WORDCLOCK_SITE = sub_wordclock
 WORDCLOCK_SITE_METHOD = local
-WORDCLOCK_DEPENDENCIES = host-vala libglib2 json-glib glib-networking libsoup libgee luajit cairo pango
+WORDCLOCK_DEPENDENCIES = host-vala libglib2 json-glib glib-networking libsoup libgee luajit cairo pango dejavu wpa_supplicant
 
 define WORDCLOCK_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 package/wordclock/S50wordclock \
