32bca47f0e9741ca73a3f0e1c18e0bb5d68d8f43
Aaron Larisch <aaron@openlarry.de>
2018-10-23 22:27:30 +0200

    Replace ntpd with chrony 3.4

--- package/dhcpcd/dhcpcd.mk
+++ package/dhcpcd/dhcpcd.mk
@@ -41,7 +41,7 @@ ifeq ($(BR2_USE_MMU),)
 DHCPCD_CONFIG_OPTS += --disable-fork
 endif
 
-DHCPCD_CONFIG_OPTS += --with-hook=ntp --with-hook=wpa_supplicant
+DHCPCD_CONFIG_OPTS += --with-hook=wpa_supplicant
 
 define DHCPCD_CONFIGURE_CMDS
 	(cd $(@D); $(TARGET_CONFIGURE_OPTS) ./configure $(DHCPCD_CONFIG_OPTS))
